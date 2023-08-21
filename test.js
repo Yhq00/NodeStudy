bf525a996615714bf35dd378355554090e610444
Yhq <1193086708@qq.com>
2023-08-21 20:46:11 +0800

    nothing

--- test.js
+++ test.js
@@ -1,12 +1,13 @@
 const index = require('./baigei_tools/index');
 
-// const dtStr = index.dateFormat(new Date());
+const dtStr = index.dateFormat(new Date());
 
-// console.log(dtStr);
+console.log(dtStr);
 
 const htmlStr = '<h1 title="abc"> 这是h1标签<span>123&nbsp</span></h1 > '
 
 const res = index.htmlEscape(htmlStr);
+console.log(res);
 
 const res1 = index.htmlUnescape(res);
 console.log(res1);
