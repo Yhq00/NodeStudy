ba52a547c0fa2504e0ab3ee777f46da559ff061b
Yhq <1193086708@qq.com>
2023-08-26 11:17:26 +0800

    docs: 学习了expressGET、POST请求及配置

--- express/03.4创建web服务器及基本的API路由接口模块.js
+++ express/03.4创建web服务器及基本的API路由接口模块.js
@@ -3,12 +3,18 @@ const express = require('express');
 //创建服务器实例对象
 const app = express();
 
+//配置解析表单数据的中间件
+app.use(express.urlencoded({ extended: false }))
+
+//配置解析JSON数据的中间件
+app.use(express.json())
+
 //导入路由模块
 const router = require('./04.API-Router')
 
 //把路由模块，注册到app上
 //app.use()用于注册中间件
-app.use('api', router)
+app.use('/api', router)
 
 //启动服务器
 app.listen(3000, () => {
--- express/04.API-Router.js
+++ express/04.API-Router.js
@@ -1,8 +1,28 @@
-const express = quire('express');
+const express = require('express');
 
 const router = express.Router();
 
 
 //在这里挂载对应的路由
 
+router.get('/get', (req, res) => {
+    //通过req.query获取到客户端通过查询字符串，发送到服务器的数据
+    const query = req.query;
+    //调用res.send方法，向客户端响应处理的结果
+    res.send({
+        status: 0, //0表示成功，1表示处理失败
+        msg: 'GET请求成功',//状态的描述
+        data: query
+    });
+})
+router.post('/post', (req, res) => {
+    //获取客户端通过请求体，发送到服务器的URL-encoded格式的数据
+    const body = req.body;
+    //调用res.send()方法，向客户端响应处理的结果
+    res.send({
+        status: 0, //0表示成功，1表示处理失败
+        msg: 'POST请求成功',//状态的描述
+        data: body  //需要响应给客户端的具体数据
+    });
+});
 module.exports = router;
